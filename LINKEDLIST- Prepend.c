ff7f0b63020bc6221d3e3ae430fe2f3205d6e41d
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2020-09-23 20:55:38 +0530

    Create LINKEDLIST- Prepend.c

---
+++ LINKEDLIST- Prepend.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+typedef struct Node Node;
+struct Node
+{
+  int val;
+  Node* next;
+};
+Node* head=NULL;
+Node* tail=NULL;
+
+void prepend(int price)
+{
+    Node* newNode=(Node*)malloc(sizeof(Node));
+    newNode->price=price;
+    newNode->next=head;
+    head=newNode;
+    if(tail==NULL)
+    {
+        tail=newNode;
+    }
+}
+int main()
+{
+  int n,value;
+  scanf("%d",&n);
+  for(int i=1;i<=n;i++)
+  {
+    scanf("%d ",&value);
+    append(value);
+  }
+  Node* ptr=head;
+  while(ptr!=NULL)
+  {
+    printf("%d->",ptr->val);
+    ptr=ptr->next;
+  }
+  printf("NULL");
+}
